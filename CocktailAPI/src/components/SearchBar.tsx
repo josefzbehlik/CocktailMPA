dcdb341653b9cf11297c350833c757fb811f55f5
josefzbehlik <josef.zbehlik.022@pslib.cz>
2024-11-08 09:27:21 +0100

    adam

--- CocktailAPI/src/components/SearchBar.tsx
+++ CocktailAPI/src/components/SearchBar.tsx
@@ -26,6 +26,11 @@ const SearchBar: FC<SearchBarProps> = ({onSearchChange}) => {
                 type="text"
                 value={search}
                 onChange={(e) => setSearch(e.target.value)}
+                onKeyDown={(e) => {
+                    if (e.key === "Enter") {
+                        onSearchChange(search);
+                    }
+                }}
             />
             {loading && <p>Loading...</p>}
             {error && <p>CHYBA: {error}</p>}
